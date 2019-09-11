110c5a29a22c6ca888f45c271770a3bf51cca1be
matt0418 <mfeldma7@gmail.com>
2019-09-11 16:09:32 -0400

    small change

--- sorted/sortedRouter.js
+++ sorted/sortedRouter.js
@@ -19,12 +19,13 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/all', async (req, res) => {
+    const topN = 1500
     try {
         const companyList = await Companies.findAll()
         companyList.sort((a, b) => { 
             return b.bayesrating - a.bayesrating
         })
-        const response = companyList 
+        const response = companyList.slice(0, topN)
         return res.status(200).json(response)
     } catch(error) {
         return res.status(500).json(error)
